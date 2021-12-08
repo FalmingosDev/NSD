f2be6798aa0ac1a7551b65500aaa03093bd4c29f
RohitChanda <89380775+FalmingosDev@users.noreply.github.com>
2021-12-08 16:21:30 +0530

    conflict

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -10,6 +10,7 @@ import { Router } from '@angular/router';
 export class ApiService {
   redirectUrl: string;
   baseUrl:string = "http://3.0.255.31/NS/php";
+  baseUrlApi:string = "http://3.0.255.31/NS/newoadmin/api";
   @Output() getLoggedInName: EventEmitter<any> = new EventEmitter();
   user_data:any;
   previewData:object;  //knockout file
