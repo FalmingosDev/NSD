7cd67188c32ff2e7171520130b76133a526d15bd
RohitChanda <89380775+FalmingosDev@users.noreply.github.com>
2022-02-08 10:33:01 +0530

    08-02-2022-change api.ts file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # NSD
 Newo Street
 ## Build Command
-ng build --output-path dist --outputHashing=none --base-href /NS/
+ng build --output-path dist --outputHashing=none --base-href //
--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -9,9 +9,7 @@ import { Router } from '@angular/router';
 })
 export class ApiService {
   redirectUrl: string;
-  // baseUrl:string = "http://3.0.255.31/NS/php";
-  baseUrl:string = "http://52.76.117.170/php"
-  // baseUrlApi:string = "http://newostreet.flamingostech.com/newoadmin/api";
+  baseUrl:string = "http://52.76.117.170/php";
   @Output() getLoggedInName: EventEmitter<any> = new EventEmitter();
   user_data:any;
   previewData:object;  //knockout file
