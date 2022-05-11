06ca1481b5ad4b9ca20e9fdb39cc70a745fa8d72
FalmingosDev <dev.flamingostech@gmail.com>
2022-05-11 18:30:38 +0530

    11-05-2022  18:30

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -27,6 +27,16 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.getCurrentLocation();  
+    const togglePassword = document.querySelector('#togglePassword');
+    const password = document.querySelector('#id_password');
+   
+    togglePassword.addEventListener('click', function (e) {
+      // toggle the type attribute
+      const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+      password.setAttribute('type', type);
+      // toggle the eye slash icon
+      this.classList.toggle('fa-eye-slash');
+  });
   }
 
   getCurrentLocation() {
