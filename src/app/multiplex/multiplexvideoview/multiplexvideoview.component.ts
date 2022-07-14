aa2732afc7972b104b01b3ee5d91929d068a89f2
FalmingosDev <89380775+FalmingosDev@users.noreply.github.com>
2022-07-13 18:23:20 -0700

    13-07-2022  18:22

--- src/app/multiplex/multiplexvideoview/multiplexvideoview.component.ts
+++ src/app/multiplex/multiplexvideoview/multiplexvideoview.component.ts
@@ -7,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MultiplexvideoviewComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+  //   $(document).ready(function() {
+  //     $("form#ratingForm").submit(function(e) 
+  //     {
+  //         e.preventDefault(); // prevent the default click action from being performed
+  //         if ($("#ratingForm :radio:checked").length == 0) {
+  //             $('#status').html("nothing checked");
+  //             return false;
+  //         } else {
+  //             $('#status').html( 'You picked ' + $('input:radio[name=rating]:checked').val() );
+  //         }
+  //     });
+  // });
+  }
 
   ngOnInit(): void {
   }
