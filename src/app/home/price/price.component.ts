c5d8b8ef11980e5d0f589b7b354309876773e632
FalmingosDev <dev.flamingostech@gmail.com>
2022-08-03 15:28:55 +0530

    03-08-2022   15:28

--- src/app/home/price/price.component.ts
+++ src/app/home/price/price.component.ts
@@ -33,7 +33,6 @@ export class PriceComponent implements OnInit {
     this.code=(<HTMLInputElement>document.getElementById("code")).value;
     
       if(this.code){
-        // (<HTMLFormElement>document.getElementById('active_btn')).disabled  = true;
         this.dataService.verifycoupon(this.code).subscribe((result)=>{
           if(result.status){
             (<HTMLFormElement>document.getElementById('active_btn')).disabled  = true;
